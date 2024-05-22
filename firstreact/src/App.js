593e28b4dbabb62ee2c6410bb156e49452fc50e9
jh214 <sswed147@naver.com>
2024-05-22 11:17:24 +0900

    12주차

--- firstreact/src/App.js
+++ firstreact/src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import NotificationList from './ch_06/NotificationList';
-import FocusButton from './FocusButton';
-import Accommodate from './ch_07/Accommodate';
+// import logo from './logo.svg';
+// import './App.css';
+// import NotificationList from './ch_06/NotificationList';
+// import FocusButton from './FocusButton';
+// import Accommodate from './ch_07/Accommodate';
+import LandingPage from './ch_09/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      <Accommodate/>
+      <LandingPage/>
     </div>
   );
 }
--- firstreact/src/index.js
+++ firstreact/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Welcome from './ch_05/Welcome';
